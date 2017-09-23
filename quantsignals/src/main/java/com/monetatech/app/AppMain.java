46718b495843abc8730a058b78481b05060c6ce7
yksi7417 <yatkasi@gmail.com>
2017-09-23 10:48:35 +0800

    compiles with AppMain - does nothing but project is building now

---
+++ quantsignals/src/main/java/com/monetatech/app/AppMain.java
@@ -0,0 +1,11 @@
+package com.monetatech.app;
+
+/**
+ * Created by asimoneta on 9/23/2017.
+ */
+public class AppMain {
+
+    public static void main(String[] args){
+        System.out.println("Application Starting... ");
+    }
+}
